d146717915f6fb26bbca8b3767f51db742063e04
Veronica Cojocari <veronicacojo@gmail.com>
2023-06-07 12:39:29 -0400

    fix: adding in line comments to separate fields and adding patient prefix to two fields

--- models/marts/staging/stg_patients.sql
+++ models/marts/staging/stg_patients.sql
@@ -9,17 +9,20 @@ final as (
     select 
         -- primary key
         id as patient_id,
+        -- details
         gender as patient_gender,
-        birthDate as patient_birth_date,
+ 
         CONCAT('MASKED_', name[SAFE_OFFSET(0)].family) as patient_last_name,
         name[SAFE_OFFSET(0)].given as patient_first_name,
-        name[SAFE_OFFSET(0)].use as name_in_use,
-        deceased.dateTime as deceased_at,
+        name[SAFE_OFFSET(0)].use as patient_name_in_use,
         address[SAFE_OFFSET(0)].country as patient_country,
         address[SAFE_OFFSET(0)].state as patient_state,
         address[SAFE_OFFSET(0)].postalCode as patient_postal_code,
         address[SAFE_OFFSET(0)].city as patient_city,
-        address[SAFE_OFFSET(0)].line as patient_address
+        address[SAFE_OFFSET(0)].line as patient_address,
+        -- timestamps
+        birthDate as patient_birth_date,
+        deceased.dateTime as patient_deceased_at
     
     from src_patients
     where name[SAFE_OFFSET(0)].use = 'official'
