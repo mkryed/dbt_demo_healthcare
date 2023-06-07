59a9418e461b6d82c5c9667cc85969f43947e364
Veronica Cojocari <veronicacojo@gmail.com>
2023-06-07 08:59:30 -0400

    feature: adding stg_patiens model and corresponding yaml with the documetation

---
+++ models/docs/_staging_docs.md
@@ -0,0 +1,51 @@
+{% docs stg_patients %}
+Staging table that defines patient details such as addresses and names.
+{% enddocs %}
+
+{% docs patient_id %}
+TODO
+{% enddocs %}
+
+{% docs patient_gender %}
+TODO
+{% enddocs %}
+
+{% docs patient_birth_date %}
+TODO
+{% enddocs %}
+
+{% docs patient_last_name %}
+TODO
+{% enddocs %}
+
+{% docs patient_first_name %}
+TODO
+{% enddocs %}
+
+{% docs name_in_use %}
+TODO
+{% enddocs %}
+
+{% docs deceased_at %}
+TODO
+{% enddocs %}
+
+{% docs patient_country %}
+TODO
+{% enddocs %}
+
+{% docs patient_state %}
+TODO
+{% enddocs %}
+
+{% docs patient_postal_code %}
+TODO
+{% enddocs %}
+
+{% docs patient_city %}
+TODO
+{% enddocs %}
+
+{% docs patient_address %}
+TODO
+{% enddocs %}
---
+++ models/marts/staging/stg_patients.sql
@@ -0,0 +1,28 @@
+with src_patients as (
+
+    select * from {{ source('demo_healthcare', 'patients_view') }}
+
+),
+
+final as (
+
+    select 
+        -- primary key
+        id as patient_id,
+        gender as patient_gender,
+        birthDate as patient_birth_date,
+        name[SAFE_OFFSET(0)].family as patient_last_name,
+        name[SAFE_OFFSET(0)].given as patient_first_name,
+        name[SAFE_OFFSET(0)].use as name_in_use,
+        deceased.dateTime as deceased_at,
+        address[SAFE_OFFSET(0)].country as patient_country,
+        address[SAFE_OFFSET(0)].state as patient_state,
+        address[SAFE_OFFSET(0)].postalCode as patient_postal_code,
+        address[SAFE_OFFSET(0)].city as patient_city,
+        address[SAFE_OFFSET(0)].line as patient_address
+    
+    from src_patients
+
+)
+
+select * from final
