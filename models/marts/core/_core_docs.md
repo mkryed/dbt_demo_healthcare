52d1d7dde31255aa409f91e67d7051507d8410c1
Mikhail <mikhail.ramyed@montrealanalytics.com>
2023-06-09 01:04:53 -0400

    fix: indentation,remove metrics,reduce CTEs

--- models/marts/core/_core_docs.md
+++ models/marts/core/_core_docs.md
@@ -16,14 +16,6 @@ A primary key of the fct_hospital_events table.
 The difference between the encounter_started_at and the encounter_ended_at
 {% enddocs %}
 
-{% docs encounter_total_by_patient_and_organization %}
-The total number of encounters by patient and organization
-{% enddocs %}
-
 {% docs procedure_duration_minutes %}
 The difference between the procedure_started_at and the procedure_ended_at
-{% enddocs %}
-
-{% docs procedure_total_by_procedure_name %}
-The total number of procedures by each procedure name
 {% enddocs %}
--- models/marts/core/fct_hospital_events.sql
+++ models/marts/core/fct_hospital_events.sql
@@ -19,9 +19,7 @@ encounters_details as (
         encounter_started_at,
         encounter_ended_at,
         timestamp_diff(timestamp(encounter_ended_at), timestamp(encounter_started_at), minute) as
-encounter_duration_minutes,
-        count(*) over (partition by patient_id, organization_id) as
-encounter_total_by_patient_and_organization
+        encounter_duration_minutes
 
 
     from encounters
@@ -36,41 +34,11 @@ procedures_details as (
         procedure_code,
         procedure_started_at,
         procedure_ended_at,
-        count(*) over (partition by procedure_code) as procedure_total,
         timestamp_diff(timestamp(procedure_ended_at), timestamp(procedure_started_at), minute) as
-procedure_duration_minutes
+        procedure_duration_minutes
 
 
     from procedures
-
-),
-
-fct_details as (
-
-    select
-        {{ dbt_utils.generate_surrogate_key(
-            ['encounters_details.encounter_id', 'procedures_details.procedure_id']
-        ) }} as hospital_sk,
-        procedures_details.procedure_id as procedure_id,
-        procedures_details.condition_id as condition_id,
-        encounters_details.encounter_id as encounter_id,
-        encounters_details.patient_id as patient_id,
-        encounters_details.organization_id as organization_id,
-        procedures_details.procedure_code as procedure_name,
-        procedures_details.procedure_started_at as procedure_started_at,
-        procedures_details.procedure_ended_at as procedure_ended_at,
-        encounters_details.encounter_started_at as encounter_started_at,
-        encounters_details.encounter_ended_at as encounter_ended_at,
-        encounters_details.encounter_duration_minutes as encounter_duration_minutes,
-        encounters_details.encounter_total_by_patient_and_organization as
-encounter_total_by_patient_and_organization,
-        procedures_details.procedure_duration_minutes as procedure_duration_minutes,
-        procedures_details.procedure_total as procedure_total_by_procedure_name
-
-
-    from encounters_details
-    left join procedures_details
-    on procedures_details.encounter_id = encounters_details.encounter_id
 
 ),
 
@@ -78,29 +46,34 @@ final as (
 
     select
         -- primary key
-        hospital_sk,
+        {{ dbt_utils.generate_surrogate_key(
+            ['encounters_details.encounter_id', 'procedures_details.procedure_id']
+        ) }} as hospital_sk,
 
         -- foreign key
-        procedure_id,
-        condition_id,
-        encounter_id,
-        patient_id,
-        organization_id,
-        procedure_name,
+        procedures_details.procedure_id,
+        procedures_details.condition_id,
+        encounters_details.encounter_id,
+        encounters_details.patient_id,
+        encounters_details.organization_id,
 
-        -- dates & timestamps
-        procedure_started_at,
-        procedure_ended_at,
-        encounter_started_at,
-        encounter_ended_at,
+        -- dates & timestamp
+        procedures_details.procedure_started_at,
+        procedures_details.procedure_ended_at,
+        encounters_details.encounter_started_at,
+        encounters_details.encounter_ended_at,
+
+        -- details
+        procedures_details.procedure_code,
 
         -- metrics
-        encounter_duration_minutes,
-        encounter_total_by_patient_and_organization,
-        procedure_duration_minutes,
-        procedure_total_by_procedure_name
+        encounters_details.encounter_duration_minutes,
+        procedures_details.procedure_duration_minutes
 
-    from fct_details
+
+    from encounters_details
+    left join procedures_details
+    on procedures_details.encounter_id = encounters_details.encounter_id
 
 )
 
