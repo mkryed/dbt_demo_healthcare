ef822856a435951b3210beb7aaba706f68c78cda
Mikhail <mikhail.ramyed@montrealanalytics.com>
2023-06-12 16:51:54 -0400

    fix: macros format - lower caps check

--- macros/age_general.sql
+++ macros/age_general.sql
@@ -1,10 +1,10 @@
 {% macro age_general(date_1, date_2 = none) %}
 
 {% if date_2 == none %}
-    date_diff(current_date(), date({{ date_1 }}), YEAR) -
+    date_diff(current_date(), date({{ date_1 }}), year) -
     if(extract(dayofyear from date({{ date_1 }})) > extract(dayofyear from current_date()), 1, 0)
 {% else %}
-    date_diff(date({{ date_2 }}), date({{ date_1 }}), YEAR) -
+    date_diff(date({{ date_2 }}), date({{ date_1 }}), year) -
     if(extract(dayofyear from date({{ date_1 }})) > extract(dayofyear from date({{ date_2 }})), 1, 0)
 {% endif %}
 
