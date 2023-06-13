df49d02e82e0dfb10b53f3e0d9457ed2f875b533
Mikhail <mikhail.ramyed@montrealanalytics.com>
2023-06-13 14:10:46 -0400

    fix: update column to string

--- models/marts/staging/stg_organizations.sql
+++ models/marts/staging/stg_organizations.sql
@@ -12,7 +12,7 @@ final as (
         --strings
         name as organization_name,
         type[SAFE_OFFSET(0)].text as organization_type,
-        address[SAFE_OFFSET(0)].line as organization_address,
+        address[SAFE_OFFSET(0)].line[0] as organization_address,
         address[SAFE_OFFSET(0)].city as organization_city,
         address[SAFE_OFFSET(0)].state as organization_state,
         address[SAFE_OFFSET(0)].country as organization_country,
