5e1d7f940986738a7b5c241f91353cd75bb76325
Cecile <43103933+cecile-cecile@users.noreply.github.com>
2023-06-15 07:27:38 -0700

    Feat: Add dimensions to fct-claims (#24)
    * feat: include condition_display field and fix doc block references
    
    * docs: fix doc block reference
    
    * feat: add condition_type and procedure_code to fct_claims
    
    * chore: fix indent

--- models/staging/stg_conditions.sql
+++ models/staging/stg_conditions.sql
@@ -15,6 +15,7 @@ renamed as (
 
         -- details
         code.coding[SAFE_OFFSET(0)].code as condition_code,
+        code.coding[SAFE_OFFSET(0)].display as condition_display,
         code.text as condition_type,
 
         -- timestamps
